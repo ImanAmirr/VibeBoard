706160b5ff5558ab086712c5730dfacc4227f4a2
Iman <emanhamza234467@gmail.com>
2026-06-24 17:22:26 +0500

    redis fix

--- redis_conn.py
+++ redis_conn.py
@@ -1,9 +1,6 @@
+import os
 import redis
 from rq import Queue
 
-redis_conn = redis.Redis(host="localhost", port=6379, decode_responses=True)
-
-from rq import Queue
-from redis_conn import redis_conn
-
+redis_conn = redis.from_url(os.getenv("REDIS_URL"))
 q = Queue(connection=redis_conn)
